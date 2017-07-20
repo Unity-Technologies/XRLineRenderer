5563babe924e58c7af1c94cada030ee76c10757f
Dylan Urquidi <dylanu@unity3d.com>
2017-07-20 16:01:48 -0700

    Convert array-based singular property to individual start/end returning properties

--- Scripts/VRLineRenderer.cs
+++ Scripts/VRLineRenderer.cs
@@ -51,11 +51,8 @@ public class VRLineRenderer : MonoBehaviour
 
     public float widthStart { get { return m_WidthStart; } }
     public float widthEnd { get { return m_WidthEnd; } }
-
-    /// <summary>
-    /// Return an array whose constituents are the start and end colors, in that order
-    /// </summary>
-    public Color[] colors { get { return new[]{m_ColorStart, m_ColorEnd}; } }
+    public Color colorStart { get { return m_ColorStart; } }
+    public Color colorEnd { get { return m_ColorEnd; } }
 
     /// <summary>
     /// Ensures the lines have all their data precached upon loading
